735ded0ca37777d0fb24f4a74d0ec4b994517196
milo-space <kilo_slacker07@icloud.com>
2024-10-24 09:31:42 +0900

    Update table.py

--- table/table.py
+++ table/table.py
@@ -1,1 +1,9 @@
+import Pyside6
 
+class Table(Pyside6.QtWidgets.QTableWidget)
+    def __init__(self, parent = None):
+        super().__init__.(parent)
+
+    def add_data(self, data):
+        self.data = data
+        
